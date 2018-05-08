ca60be21516b217a7fd2907b1cd304a6a16beb4e
michael hohl <mhohl@rocketmail.com>
2018-05-08 18:29:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,15 +1,19 @@
 class Application
 
+  @@items = ["Apples","Carrots","Pears"]
+
   def call(env)
     resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
-    if Time.now.hour < 12
-      resp.write "Good Morning!"
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
     else
-      resp.write "Good Afternoon!"
+      resp.write "Path Not Found"
     end
 
     resp.finish
   end
-
 end
